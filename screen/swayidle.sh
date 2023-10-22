295799914c849f9138e615062126d30998879a05
dhonus <dhonus@tuta.io>
2023-10-22 12:02:58 +0200

    idle script; color scheme change

--- screen/swayidle.sh
+++ screen/swayidle.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 exec swayidle -w \
-    timeout 300 "$DIR/idle.sh" \
+    timeout 200 "$DIR/idle.sh" \
         resume "$DIR/de_idle.sh"
